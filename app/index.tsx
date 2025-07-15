02c81c70d044028bf1d2b8ded77ba94b86763fd3
SoundtrkSamurai <furtimat3@yahoo.com>
2025-07-14 18:23:31 -0700

    feat: Integrate FlashList for optimized product rendering and add React Query DevTools support

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,33 @@
+import useGetCategories from "@/hooks/api/useGetCategories";
 import useGetProducts from "@/hooks/api/useGetProducts";
-import { Text, View } from "react-native";
+import { Product } from "@/types/interfaces";
+import { FlashList } from "@shopify/flash-list";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
-  const { products, isLoading, error } = useGetProducts();
+  const { products } = useGetProducts();
 
-  if (isLoading) {
-    return <Text>Loading...</Text>;
-  }
-
-  if (error) {
-    return <Text>Error loading products</Text>;
-  }
+  const { categories } = useGetCategories();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <FlashList
+        data={products ?? []}
+        renderItem={({ item }: { item: Product }) => (
+          <View style={{ padding: 20 }}>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+        keyExtractor={(item: Product) => item.id?.toString() ?? item.title}
+        estimatedItemSize={50}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
